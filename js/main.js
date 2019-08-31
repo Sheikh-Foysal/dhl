cf912668393ab1320d37a7f29c980b848a2252ca
Sheikh-Foysal <openoway@gmail.com>
2019-08-31 17:48:06 +0600

    home page full done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
     new WOW().init();
     GalleryFunction();
     HeroSliderOwlCarouselFunction();
+    FeaturesSliderOwlCarouselFunction();
+    PartnerSliderOwlCarouselFunction();
     SlickMobileMenuFunction();
 
 });
@@ -59,6 +61,69 @@ function HeroSliderOwlCarouselFunction(){
 }
 
 
+
+
+// Owl Carousel Function
+function FeaturesSliderOwlCarouselFunction(){
+    $('#features_services_slider').owlCarousel({
+        loop:true,
+        margin:false,
+        nav:false,
+        margin: 30,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 3000,
+        navSpeed: 4000,
+        dotsSpeed: 4000,
+        dragEndSpeed: 1000,
+        navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        },
+    });
+}
+
+
+
+
+
+// Owl Carousel Function
+function PartnerSliderOwlCarouselFunction(){
+    $('#partner_slider').owlCarousel({
+        loop:true,
+        margin:false,
+        nav:false,
+        margin: 30,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 3000,
+        navSpeed: 4000,
+        dotsSpeed: 4000,
+        dragEndSpeed: 1000,
+        navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:4
+            },
+            1000:{
+                items:6
+            }
+        },
+    });
+}
 
 // Slick Mobile Menu Function
 function SlickMobileMenuFunction(){
